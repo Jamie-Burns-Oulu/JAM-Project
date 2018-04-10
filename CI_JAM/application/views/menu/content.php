82722d87b0ba4584f510e9f018ee1686018b48d8
Máté Gulyás <t7guma00@students.oamk.fi>
2018-04-10 14:33:24 +0300

    content change

--- CI_JAM/application/views/menu/content.php
+++ CI_JAM/application/views/menu/content.php
@@ -1,14 +1,11 @@
 <?php
-
+$user_id=$this->session->userdata('user_id');
+if (($user_id) && ($_SESSION['user_name']) == "admin"){
     $this->load->view('menu/header_staff');
-
 }
 else{
-
     $this->load->view('menu/header');
-
 }
 $this->load->view($page);
 $this->load->view('menu/footer');
-
  ?>
